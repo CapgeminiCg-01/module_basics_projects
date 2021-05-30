24b3eb1bc86a85676f1ff0ebe3e232a4649b0824
MohitJindal99 <mohitjd1999@gmail.com>
2021-05-30 17:07:08 +0530

    v1

---
+++ calculator.js
@@ -0,0 +1,8 @@
+let titile = "Project Title";
+
+let sum = (n1, n2) => n1 + n2;
+
+let diff = (n1, n2) => n2 - n1;
+
+//public 
+export {sum, diff};
---
+++ main.js
@@ -0,0 +1,7 @@
+// local module must starts with ./
+import {sum} from "./calculator.js";
+
+console.log("hello world");
+
+let total = sum(10,20);
+console.log(total);
